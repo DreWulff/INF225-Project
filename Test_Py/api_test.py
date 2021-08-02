41c53500464b0fad7c61330cfc418d9ec37f0309
DreWulff <awulff98@gmail.com>
2021-08-02 04:02:02 -0400

    Finished API calls in .py

--- Test_Py/api_test.py
+++ Test_Py/api_test.py
@@ -55,7 +55,8 @@ print("\n10. añadirHabilitacion(555555555-2, 10101010-A, 1, False)")
 json_value = response.json()
 print(json_value)
 
-response = requests.put("http://3.235.42.11:3000/maquina/" + str(json_value.get('insertId')))
+myobj = {'habilitado': True}
+response = requests.put("http://3.235.42.11:3000/habilitacion/" + str(json_value.get('insertId')), data=myobj)
 print("\n11. actualizarHabilitacion("+str(json_value.get('insertId'))+")")
 json_value = response.json()
 print(json_value)
@@ -69,31 +70,40 @@ print("\n12. añadirHabilitacion(234234324-1, 199999999-A, 3, False)")
 json_value = response.json()
 print(json_value)
 
-response = requests.put("http://3.235.42.11:3000/maquina/" + str(json_value.get('insertId')))
+myobj = {'habilitado': True}
+response = requests.put("http://3.235.42.11:3000/habilitacion/" + str(json_value.get('insertId')), data=myobj)
 print("\n13. actualizarHabilitacion("+str(json_value.get('insertId'))+")")
 json_value = response.json()
 print(json_value)
 
-#response = requests.get("http://3.235.42.11:3000/maquina")
-#print("1. listarMaquinas")
-#print(response.text)
-#
-#response = requests.get("http://3.235.42.11:3000/maquina")
-#print("1. listarMaquinas")
-#print(response.text)
-#
-#response = requests.get("http://3.235.42.11:3000/maquina")
-#print("1. listarMaquinas")
-#print(response.text)
-#
-#response = requests.get("http://3.235.42.11:3000/maquina")
-#print("1. listarMaquinas")
-#print(response.text)
-#
-#response = requests.get("http://3.235.42.11:3000/maquina")
-#print("1. listarMaquinas")
-#print(response.text)
-#
+myobj = {'link': "testlink_urlyep.com"}
+response = requests.put("http://3.235.42.11:3000/recurso/1", data=myobj)
+print("\n14. actualizarRecurso(1, testlink_urlyep.com)")
+print(response.text)
+
+myobj = {'link': "testlink_urlyep.com"}
+response = requests.put("http://3.235.42.11:3000/recurso/1", data=myobj)
+print("\n15. actualizarRecurso(1, https://www.youtube.com/watch?v=Lf4IezGvFTc&ab_channel=YourDankMemeDoner)")
+print(response.text)
+
+myobj = {'link': "example-link.org/yep"}
+response = requests.put("http://3.235.42.11:3000/recurso/2", data=myobj)
+print("\n16. actualizarRecurso(2, example-link.org/yep)")
+print(response.text)
+
+myobj = {'link': "https://www.youtube.com/watch?v=pIg6JjoXQ0A&t=192s&ab_channel=RegisKillbin"}
+response = requests.put("http://3.235.42.11:3000/recurso/2", data=myobj)
+print("\n17. actualizarRecurso(2, https://www.youtube.com/watch?v=pIg6JjoXQ0A&t=192s&ab_channel=RegisKillbin)")
+print(response.text)
+
+myobj = {'nombre': 'Spam Machine',
+         'ubicacion': 'Spam',
+         'tipo_maquina': 5}
+response = requests.post("http://3.235.42.11:3000/maquina", data=myobj)
+print("\n18. añadirMaquina('Spam Machine', 'Spam', 5)")
+json_value = response.json()
+print(json_value)
+
 myobj = {'nombre': 'Nueva Maquina',
          'ubicacion': 'Nuevo lugar',
          'tipo_maquina': 3}
