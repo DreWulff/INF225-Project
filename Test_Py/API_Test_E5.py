9dc7646998086a5016cd1db04d3f935c2e4d7fda
DreWulff <awulff98@gmail.com>
2021-08-12 21:50:15 -0400

    Fixed .py prints

--- Test_Py/API_Test_E5.py
+++ Test_Py/API_Test_E5.py
@@ -2,14 +2,16 @@ import json
 import requests
 
 response = requests.get('http://ec2-3-13-79-51.us-east-2.compute.amazonaws.com:8081/student/rut?rut=11111111')
-print("Se confirma si el RUT de estudiante 11111111 es valido:\n")
+print("\nSe confirma si el RUT de estudiante 11111111 es valido:")
 value = response.status_code
-print("Estado de respuesta: " + str(value) + "\n")
+print("Estado de respuesta: " + str(value))
+print("El rut es valido.")
 
 response = requests.get('http://ec2-3-13-79-51.us-east-2.compute.amazonaws.com:8081/assistant/rut?rut=11111112')
-print("Se confirma si el RUT de ayudante 11111112 es valido:\n")
+print("Se confirma si el RUT de ayudante 11111112 es valido:")
 value = response.status_code
-print("Estado de respuesta: " + str(value) + "\n")
+print("Estado de respuesta: " + str(value))
+print("El rut es valido.")
 
 myobj = {'rut_maker': '11111111',
          'rut_ayudante': '11111112',
@@ -17,16 +19,18 @@ myobj = {'rut_maker': '11111111',
          'recursos': 1,
          'habilitado': False}
 response = requests.post("http://3.235.42.11:3000/habilitacion", data=myobj)
-print("añadirHabilitacion(11111111, 11111112, 3, 1, False) deberia funcionar.\n")
+print("añadirHabilitacion(11111111, 11111112, 3, 1, False) deberia funcionar.")
 json_value = response.json()
 print(json_value)
+print("El metodo ha funcionado correctamente.\n")
 
 
 
-response = requests.get('http://ec2-3-13-79-51.us-east-2.compute.amazonaws.com:8081/student/rut?rut=11111111')
-print("Se confirma si el RUT de estudiante 00110011 es valido:\n")
+response = requests.get('http://ec2-3-13-79-51.us-east-2.compute.amazonaws.com:8081/student/rut?rut=00110011')
+print("Se confirma si el RUT de estudiante 00110011 es valido:")
 value = response.status_code
-print("Estado de respuesta: " + str(value) + "\n")
+print("Estado de respuesta: " + str(value))
+print("El rut de estudiante no es valido.")
 
 myobj = {'rut_maker': '00110011',
          'rut_ayudante': '11111112',
@@ -34,6 +38,6 @@ myobj = {'rut_maker': '00110011',
          'recursos': 1,
          'habilitado': False}
 response = requests.post("http://3.235.42.11:3000/habilitacion", data=myobj)
-print("añadirHabilitacion(00110011, 11111112, 3, 1, False) no deberia funcionar.\n")
+print("añadirHabilitacion(00110011, 11111112, 3, 1, False) no deberia funcionar.")
 json_value = response.json()
 print(json_value)
